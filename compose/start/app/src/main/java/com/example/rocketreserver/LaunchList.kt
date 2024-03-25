7ad1be2e3b6236584454dab312eba2ef340dc09a
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-03-24 18:04:38 -0600

    // WIP: Configure LaunchListAdapter

--- compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -11,19 +11,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Optional
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var launchList by remember { mutableStateOf(emptyList<LaunchListQuery.Launch>()) }
     LaunchedEffect(Unit) {
         val response = apolloClient.query(LaunchListQuery()).execute()
         Log.d("LaunchList", "Success ${response.data}")
